e89ba92a5d544f2194cb79940d90d45dd1aedcf2
Rémi Robichet <rrobichet@infinitestudio.fr>
2026-02-03 15:37:55 +0100

    test

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
   colorMode: { classSuffix: "" },
 
   content: {
-    experimental: { nativeSqlite: true },
+    experimental: { sqliteConnector: "better-sqlite3" },
     build: {
       markdown: {
         highlight: {
